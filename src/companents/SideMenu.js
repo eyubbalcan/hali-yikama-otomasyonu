110ac61bfed7bc84caf9ad3a2920acce1ef5f1c3
eyubbalcan <balcan1905@gmail.com>
2023-08-21 23:20:49 +0300

    Devam

--- src/companents/SideMenu.js
+++ src/companents/SideMenu.js
@@ -102,29 +102,3 @@ export default function SideMenu(props) {
   );
 }
 
-// import { Routes, Route, Link, NavLink } from "react-router-dom";
-// import Home from "../../pages/Home";
-// import Contact from "../../pages/Contact";
-// import Blog from "../../pages/Blog";
-// import "./Sidebar.css";
-
-// function Sidebar() {
-//   return (
-//     <>
-//       <nav className="sidebar">
-//         <NavLink to="/" style={({ isActive }) => ({})}>
-//           Anasayfa
-//         </NavLink>
-//         <NavLink to="/contact">Contact</NavLink>
-//         <NavLink to="/blog">Blog</NavLink>
-//       </nav>
-//       <Routes>
-//         <Route path="/" element={<Home />}></Route>
-//         <Route path="/contact" element={<Contact />}></Route>
-//         <Route path="/blog" element={<Blog />}></Route>
-//       </Routes>
-//     </>
-//   );
-// }
-
-// export default Sidebar;
